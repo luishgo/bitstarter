6eb241bdf4ba7822c48d1f58420e8771b162d5de
Luis HGO <luishgo@gmail.com>
2013-07-09 02:46:16 -0300

    HW3 completed

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,6 +37,10 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertURLExists = function(url) {
+    return url.toString();
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -44,8 +49,8 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile) {    
+    var $ = htmlfile;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -54,6 +59,11 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+
+var printResults = function(checkJson) {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -65,10 +75,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>', 'Path to index.html', clone(assertURLExists), null)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url) {
+      rest.get(program.url).on('complete', function(result, response) {
+        if (result instanceof Error) {
+          console.error('Error: ' + response.message);
+        } else {
+          var $ = cheerio.load(result);
+          var checkJson = checkHtmlFile($, program.checks);
+          printResults(checkJson);
+        }
+      });
+    } else {
+      var $ = cheerioHtmlFile(program.file);
+      var checkJson = checkHtmlFile($, program.checks);
+      printResults(checkJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
